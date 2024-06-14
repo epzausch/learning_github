b60cc68358d6c48397c2355e555a43669e396058
Elias <epzausch@ncsu.edu>
2024-06-14 12:33:50 -0400

    First sentence

--- hello_world.py
+++ hello_world.py
@@ -2,3 +2,6 @@ print("hello world")
 
 print("it was a dark and stormy night, all through the country side horse hooves hit the ground like thunder")
 print("thunder then lightning raced b")
+
+print("\nChapter 2\n")
+print("\tand all through the nite the man cried wolf")
